9c530731e6781673cbd0f4f8f491f499d039c394
Akash Maurya <akashmauryame@gmail.com>
2025-09-24 11:05:22 +0530

    working on the gateway service

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -1,8 +1,31 @@
-import os, gridfs, pika, json
+import os
+import gridfs
+import pika
+import json
 from flask import Flask, request
-from flaask_pymongo import PyMongo
+from flask_pymongo import PyMongo
+
+# custom modules
 from auth import validate
 from auth_svc import access
 from storage import util
 
+# creating a flask application instance
 server = Flask(__name__)
+
+server.config("MONGO_URI") = "mongodb://host.minikube.internal:27017/videos"
+
+# initializing PyMongo with the Flask app instance
+mongo = PyMongo(server)
+
+fs = gridfs.GridFS(mongo.db)
+
+# setting up RabbitMQ connection 
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+    
+
